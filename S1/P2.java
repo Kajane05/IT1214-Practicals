5dd3c1efd5f96b0c8574690c11636bb5c44ec3ed
Kajane05 <kajekajane@gmail.com>
2025-05-04 08:57:29 +0530

    Add files via upload

---
+++ S1/P2.java
@@ -0,0 +1,12 @@
+import java.io.*; 
+
+class MySecondClass{ 
+    
+	public static void main(String[] args){
+		System.out.println("The first argument is "+args[0]);
+		System.out.println("The second argument is "+args[1]);
+		System.out.println("The second argument is "+args[2]);
+	
+		System.out.println(args[0]+" "+args[2]+" "+args[1]);
+	} 
+}
